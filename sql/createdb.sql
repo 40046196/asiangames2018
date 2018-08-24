c9179163682e7191105416b96bd1a2ba4c0f511b
lionmoons <leo.sudarma@gmail.com>
2018-08-24 08:42:26 -0400

    WebParse List
    Testing scrapping Country List and  Sport List in Asian Games 2018.

---
+++ sql/createdb.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE `asiangames2018` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+CREATE TABLE `country` (
+  `countryId` varchar(3) NOT NULL,
+  `countryName` varchar(45) DEFAULT NULL,
+  `countryFlag` blob,
+  PRIMARY KEY (`countryId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `sport` (
+  `sportId` varchar(3) NOT NULL,
+  `sportName` varchar(45) DEFAULT NULL,
+  `sportIcon` blob,
+  PRIMARY KEY (`sportId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `athlete` (
+  `athleteId` varchar(7) NOT NULL,
+  `athleteName` varchar(25) NOT NULL,
+  'surname' varchar(25) DEFAULT NULL,
+  `birthdate` varchar(10) NOT NULL,
+  `countryId` varchar(3) NOT NULL,
+  `sportId` varchar(7) DEFAULT NULL,
+  `height` int(11) DEFAULT NULL,
+  `weight` int(11) DEFAULT NULL,
+  `beginning` varchar(200) DEFAULT NULL,
+  `coach` varchar(200) DEFAULT NULL,
+  `memorable` varchar(200) DEFAULT NULL,
+  `influence` varchar(200) DEFAULT NULL,
+  `nickname` varchar(200) DEFAULT NULL,
+  `language` varchar(200) DEFAULT NULL,
+  `hobbies` varchar(200) DEFAULT NULL,
+  'photo' blob, 
+  PRIMARY KEY (`athleteId`,`countryId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `athleteHighlight` (
+  `athleteId` varchar(7) NOT NULL,
+  `eventName` varchar(50) NOT NULL,
+  `rank` varchar(10) NOT NULL,
+  `event` varchar(3) NOT NULL,
+  `year` varchar(7) DEFAULT NULL,
+  `location` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`athleteId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `athleteSocial` (
+  `athleteId` varchar(7) NOT NULL,
+  `socialmedia` varchar(50) NOT NULL,
+  PRIMARY KEY (`athleteId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
--- src/main/java/com/asiangames2018/App.java
+++ src/main/java/com/asiangames2018/App.java
@@ -1,5 +1,10 @@
 package com.asiangames2018;
 
+import com.jaunt.Element;
+import com.jaunt.Elements;
+import com.jaunt.JauntException;
+import com.jaunt.UserAgent;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,26 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!g" );
+        try{
+      	  UserAgent userAgent = new UserAgent();                       //create new userAgent (headless browser).
+      	  userAgent.settings.checkSSLCerts = false;
+      	  userAgent.visit("https://en.asiangames2018.id/athletes/page/1/");         //visit a url  
+ //     	  System.out.println(userAgent.doc.innerHTML());               //print the document as HTML
+      	  
+      	  Elements elements = userAgent.doc.findEvery("<select>"); 
+      	  Elements sportsElements = elements.getElement(0).findEvery("<option>");
+      	  
+      	  
+      	  System.out.println(elements.size());
+      	  
+      	  for (Element element : sportsElements) {
+      		  System.out.println("Every OPtion: " + element.getAt("value") + " ==== "  +  element.getChildText());
+      	  }
+      	}
+      	catch(JauntException e){         //if an HTTP/connection error occurs, handle JauntException.
+      	  System.err.println(e);
+      	}        
+        
     }
 }
