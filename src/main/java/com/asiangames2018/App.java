c2230481e8ca28de99820669c13612f8d1d9b6ae
lionmoons <leo.sudarma@gmail.com>
2018-08-24 04:21:44 -0400

    Initial commit

---
+++ src/main/java/com/asiangames2018/App.java
@@ -0,0 +1,13 @@
+package com.asiangames2018;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
