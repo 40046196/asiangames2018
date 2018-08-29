7e3cbe935a1965783b36dffde1bf17c41351d4db
Raymart De Guzman <tramyardg@gmail.com>
2018-08-29 13:10:35 -0400

    UNIT TEST: testGetJDBCProperties()

--- test/com/asiangames2018/TestConnection.java
+++ test/com/asiangames2018/TestConnection.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,13 +13,25 @@ import org.junit.Test;
 import com.asiangames2018.util.DAOUtil;
 import com.asiangames2018.util.GeneralLogging;
 
-public class TestConnection extends DAOUtil {
+import junit.framework.TestCase;
+
+public class TestConnection extends TestCase {
     private Logger logger = GeneralLogging.getLogger();
 
     @Test
-    public void test() {
+    public void testGetJDBCProperties() {
+	Properties properties = null;
+	properties = DAOUtil.getJDBCProperties();
+	String className = properties.getProperty("classname");
+        String driverManager = properties.getProperty("drivermanager");
+	assertEquals("com.mysql.jdbc.Driver", className);
+        assertNotNull(driverManager);
+    }
+    
+    @Test
+    public void testGetConnection() {
 	Connection connection = null;
-	connection = super.getConnection();
+	connection = DAOUtil.getConnection();
 	try {
 	    // check if connection to MySQL DB is valid under 5 seconds
 	    // try using invalid user name or password in dao.properties 
@@ -28,4 +41,5 @@ public class TestConnection extends DAOUtil {
 	    logger.log(Level.WARNING, TestConnection.class.getName() + ": invalid connection", e);
 	}
     }
+    
 }
