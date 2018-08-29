c187f972d47c657dc9e484839a851acfacd750e5
Raymart De Guzman <tramyardg@gmail.com>
2018-08-29 13:40:28 -0400

    UNIT TEST: renamed TestConnection to TestDBConnection

--- test/com/asiangames2018/TestDBConnection.java
+++ test/com/asiangames2018/TestDBConnection.java
@@ -1,7 +1,5 @@
 package com.asiangames2018;
 
-import static org.junit.Assert.*;
-
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Properties;
@@ -15,7 +13,7 @@ import com.asiangames2018.util.GeneralLogging;
 
 import junit.framework.TestCase;
 
-public class TestConnection extends TestCase {
+public class TestDBConnection extends TestCase {
     private Logger logger = GeneralLogging.getLogger();
 
     @Test
@@ -38,7 +36,7 @@ public class TestConnection extends TestCase {
 	    // this test will fail as a result 
 	    assertEquals(true, connection.isValid(5));
 	} catch (SQLException e) {
-	    logger.log(Level.WARNING, TestConnection.class.getName() + ": invalid connection", e);
+	    logger.log(Level.WARNING, TestDBConnection.class.getName() + ": invalid connection", e);
 	}
     }
     
