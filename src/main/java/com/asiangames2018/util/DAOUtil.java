2a5e79fc3dac0c00a62ba51c78082144cddd6521
Raymart De Guzman <tramyardg@gmail.com>
2018-09-30 22:59:57 -0400

    DAOUtil ctrl+shift+f

--- src/main/java/com/asiangames2018/util/DAOUtil.java
+++ src/main/java/com/asiangames2018/util/DAOUtil.java
@@ -7,67 +7,58 @@ import java.util.Properties;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-import javax.swing.JDialog;
-
 /***********************************
  * @author Leo Sudarma
- * @Date  March 13, 2012,   5:02 a.m
- * Description:  This is the Database Access Object (DAO) Utility.
- *               This function will give you the instance of the connection to  the database.
- *               The location of the database is set in the properties folder, "properties\dao.properties"
- *               So, whenever you change your database into MySQL, Microsoft SQL, or JAVA DB, 
- *               you no need to recompile your program, as long your database has the same table name and field name. :D
- *               
- *http://www.baselogic.com/blog/solved-java-net-bindexception-address-use-connect-issue-windows/
+ * @Date March 13, 2012, 5:02 a.m Description: This is the Database Access
+ *       Object (DAO) Utility. This function will give you the instance of the
+ *       connection to the database. The location of the database is set in the
+ *       properties folder, "properties\dao.properties" So, whenever you change
+ *       your database into MySQL, Microsoft SQL, or JAVA DB, you no need to
+ *       recompile your program, as long your database has the same table name
+ *       and field name. :D
+ * 
+ *       http://www.baselogic.com/blog/solved-java-net-bindexception-address-use-connect-issue-windows/
  */
 
 public class DAOUtil {
-	
-	
-	/*****************************************************************
-	 * Loading the parameter from dao.properties files into class Properties.
-	 * @return  Properties 
-	 */
-	public static Properties getJDBCProperties() {
-		properties = new Properties();
-		try {
-			properties.load(new FileInputStream("properties\\dao.properties"));  // the folder following your working project..
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return properties;
+
+    /*****************************************************************
+     * Loading the parameter from dao.properties files into class Properties.
+     * 
+     * @return Properties
+     */
+    public static Properties getJDBCProperties() {
+	properties = new Properties();
+	try {
+	    // the folder following your working project..
+	    properties.load(new FileInputStream("properties\\dao.properties"));
+	} catch (IOException e) {
+	    e.printStackTrace();
 	}
-	
-	
-	  /**
-	   * Gets the connection attribute of the DAOUtil class
-	   *
-	   * @return   The connection value
-	   */
-	  public static Connection getConnection() {
-		
-		Connection connection = null;
-		  
-	    if (connection == null) {
-	      try {
-	        getJDBCProperties();
-	        
-	        String className = properties.getProperty("classname");
-	        String driverManager = properties.getProperty("drivermanager");
-	        
-//	        Class.forName(className).newInstance();	        
-	        connection =  DriverManager.getConnection(driverManager);
-	        
-	        connection.setAutoCommit(true);
-	      } catch (Exception ex) {
-	    	  ex.printStackTrace();
-	      }
-	    }
-	    return connection;
-	  }
-	  
+	return properties;
+    }
 
-	private static  Properties properties = null;
-	
+    /**
+     * Gets the connection attribute of the DAOUtil class
+     *
+     * @return The connection value
+     */
+    public static Connection getConnection() {
+	Connection connection = null;
+	try {
+	    getJDBCProperties();
+	    String className = properties.getProperty("classname");
+	    String driverManager = properties.getProperty("drivermanager");
+
+	    // Class.forName(className).newInstance();
+	    connection = DriverManager.getConnection(driverManager);
+	    connection.setAutoCommit(true);
+	} catch (Exception ex) {
+	    ex.printStackTrace();
+	}
+	return connection;
+    }
+
+    private static Properties properties = null;
+
 }
-
